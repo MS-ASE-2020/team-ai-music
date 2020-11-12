b7b8e4b926dadb210e13ca097909fbdb745b8e6b
Fandahao <fandahao17@mail.ustc.edu.cn>
2020-11-12 23:25:48 +0800

    Add CORS headers in backend

--- ai_music_backend/README.md
+++ ai_music_backend/README.md
@@ -1,12 +1,14 @@
 # How to run backend
 
-Now, the backend only depends on Django:
+Now, the backend only depends on Django and corsheaders:
 
 ```bash
-pip3 install django
+pip3 install django corsheaders
 ```
 
 After the installation, run the following commands to update the database (We are currently using sqlite):
+
+**NOTE that every time you pull this repo, you need to do migration!**
 
 ```bash
 python3 manage.py makemigrations
--- ai_music_backend/ai_music_backend/settings.py
+++ ai_music_backend/ai_music_backend/settings.py
@@ -37,14 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'backend',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -119,3 +121,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
+CORS_ORIGIN_WHITELIST = [
+'http://localhost:3000',
+'http://localhost:8000',
+'http://localhost:8080',
+]
